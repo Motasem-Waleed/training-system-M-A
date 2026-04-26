223f40bad46bc36d6011d5bfcd8f0e45e42ba7d4
Motasem Waleed <22210018@students.hebron.edu>
2026-04-26 12:50:30 +0300

    Update project

--- frontend/src/pages/Admin/ActivityLogs/ActivityLogsList.jsx
+++ frontend/src/pages/Admin/ActivityLogs/ActivityLogsList.jsx
@@ -123,7 +123,7 @@ export default function ActivityLogsList() {
               <th>الحدث</th>
               <th>الوصف</th>
               <th>IP</th>
-              <th>التاريخ</th>
+              <th> gdhdj التاريخ</th>
             </tr>
           </thead>
           <tbody>
